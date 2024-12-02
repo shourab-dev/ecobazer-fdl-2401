e2542b1b2aeaf8dbe2dfdfd0997e12be6ee17cf4
shourab <shourab.webdev@gmail.com>
2024-12-02 17:53:23 +0600

    Eco bazer full nav done

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    //* Search SM 
+    $("#smSearchBtn").click(function(){
+        $(".searchSm").addClass('active');
+    });
+    $('.searchSm .closeBtn').click(function(){
+        $(".searchSm").removeClass('active')
+    })
+
+
+})
+
+
+
+// $(function(){
+
+// })
